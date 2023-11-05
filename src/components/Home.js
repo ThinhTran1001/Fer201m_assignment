2b6a0880e45217b8c471f5ac72920642c854234d
ThinhTran1001 <thinhtran9797yb@gmail.com>
2023-11-05 18:30:11 +0700

    get product home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,24 @@
-import React from 'react'
-import { Col, Container, Row, Card, Image, Button } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react';
+import { Col, Container, Row, Card, Image, Button } from 'react-bootstrap';
+import axios from 'axios';
 
 export default function Home() {
+    const [product, setProduct] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const productRes = await axios.get("http://localhost:9999/product");
+                setProduct(productRes.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
-        <div style={{ marginTop: '15px' }}>
+        <div style={{ marginTop: '25px' }}>
             <Container>
                 <Row>
                     <Col md={6}><Button style={{ backgroundColor: 'rgb(220, 100, 11)', color: 'white', fontWeight: 'bolder' }} variant="outline-warning">ĐIỆN THOẠI NỔI BẬT NHẤT</Button></Col>
@@ -18,156 +33,129 @@ export default function Home() {
                         </Row>
                     </Col>
                 </Row>
-                <Row style={{ marginTop: '5px' }}>
-                    <Col xs={12} sm={6} md={3} style={{ marginTop: '15px' }} >
-                        <Card style={{ width: '18rem', textAlign: 'center' }}>
-                            <Card.Img src="./images/product/iphone14.png" />
-                            <Card.Body>
-                                <Card.Title style={{ textAlign: 'center' }}>Iphone 14 128GB </Card.Title>
-                                <Card.Text>
-                                    <Row>
-                                        <Col>26.590.000đ</Col>
-                                    </Row>
-                                    <Row>
-                                        <Col md={6}><Image src="./images/cart/heart.svg" style={{ height: " 1.5 rem" }}></Image></Col>
-                                        <Col md={6}><Image src="./images/product/star.svg" style={{ height: " 25px", width: '25px' }}></Image></Col>
-                                    </Row>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col xs={12} sm={6} md={3} style={{ marginTop: '15px' }}>
-                        <Card style={{ width: '18rem', textAlign: 'center' }}>
-                            <Card.Img src="./images/product/iphone13.png" />
-                            <Card.Body>
-                                <Card.Title style={{ textAlign: 'center' }}>Iphone 13 128GB</Card.Title>
-                                <Card.Text>
-                                    <Row>
-                                        <Col>26.590.000đ</Col>
-                                    </Row>
-                                    <Row>
-                                        <Col md={6}><Image src="./images/cart/heart.svg" style={{ height: " 1.5 rem" }}></Image></Col>
-                                        <Col md={6}><Image src="./images/product/star.svg" style={{ height: " 25px", width: '25px' }}></Image></Col>
-                                    </Row>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col xs={12} sm={6} md={3} style={{ marginTop: '15px' }}>
-                        <Card style={{ width: '18rem', textAlign: 'center' }}>
-                            <Card.Img src="./images/product/iphone14plus.png" />
-                            <Card.Body>
-                                <Card.Title style={{ textAlign: 'center' }}>Iphone 14 Plus 128GB</Card.Title>
-                                <Card.Text>
-                                    <Row>
-                                        <Col>26.590.000đ</Col>
-                                    </Row>
-                                    <Row>
-                                        <Col md={6}><Image src="./images/cart/heart.svg" style={{ height: " 1.5 rem" }}></Image></Col>
-                                        <Col md={6}><Image src="./images/product/star.svg" style={{ height: " 25px", width: '25px' }}></Image></Col>
-                                    </Row>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col xs={12} sm={6} md={3} style={{ marginTop: '15px' }}>
-                        <Card style={{ width: '18rem', textAlign: 'center' }}>
-                            <Card.Img src="./images/product/iphone13promax.png" />
-                            <Card.Body>
-                                <Card.Title style={{ textAlign: 'center' }}>Iphone 13 Pro Max 256GB</Card.Title>
-                                <Card.Text>
-                                    <Row>
-                                        <Col>26.590.000đ</Col>
-                                    </Row>
-                                    <Row>
-                                        <Col md={6}><Image src="./images/cart/heart.svg" style={{ height: " 1.5 rem" }}></Image></Col>
-                                        <Col md={6}><Image src="./images/product/star.svg" style={{ height: " 25px", width: '25px' }}></Image></Col>
-                                    </Row>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col xs={12} sm={6} md={3} style={{ marginTop: '15px' }}>
-                        <Card style={{ width: '18rem', textAlign: 'center' }}>
-                            <Card.Img style={{ width: '286px', height: '286px' }} src="./images/product/ssgs23ultra.png" />
-                            <Card.Body>
-                                <Card.Title style={{ fontSize: '16px' }}>Samsung Galaxy S23 Ultra 256GB</Card.Title>
-                                <Card.Text>
-                                    <Row>
-                                        <Col>26.590.000đ</Col>
-                                    </Row>
-                                    <Row>
-                                        <Col md={6}><Image src="./images/cart/heart.svg" style={{ height: " 1.5 rem" }}></Image></Col>
-                                        <Col md={6}><Image src="./images/product/star.svg" style={{ height: " 25px", width: '25px' }}></Image></Col>
-                                    </Row>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col xs={12} sm={6} md={3} style={{ marginTop: '15px' }}>
-                        <Card style={{ width: '18rem', textAlign: 'center' }}>
-                            <Card.Img style={{ width: '286px', height: '286px' }} src="./images/product/ssgzflip5.png" />
-                            <Card.Body>
-                                <Card.Title style={{ fontSize: '16px' }}>Samsung Galaxy Z Flip 5 512GB</Card.Title>
-                                <Card.Text>
-                                    <Row>
-                                        <Col>26.590.000đ</Col>
-                                    </Row>
-                                    <Row>
-                                        <Col md={6}><Image src="./images/cart/heart.svg" style={{ height: " 1.5 rem" }}></Image></Col>
-                                        <Col md={6}><Image src="./images/product/star.svg" style={{ height: " 25px", width: '25px' }}></Image></Col>
-                                    </Row>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col xs={12} sm={6} md={3} style={{ marginTop: '15px' }}>
-                        <Card style={{ width: '18rem', textAlign: 'center' }}>
-                            <Card.Img src="./images/product/iphone11promax.png" />
-                            <Card.Body>
-                                <Card.Title style={{ textAlign: 'center' }}>Iphone 11 Pro Max 128GB</Card.Title>
-                                <Card.Text>
-                                    <Row>
-                                        <Col>26.590.000đ</Col>
-                                    </Row>
-                                    <Row>
-                                        <Col md={6}><Image src="./images/cart/heart.svg" style={{ height: " 1.5 rem" }}></Image></Col>
-                                        <Col md={6}><Image src="./images/product/star.svg" style={{ height: " 25px", width: '25px' }}></Image></Col>
-                                    </Row>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col xs={12} sm={6} md={3} style={{ marginTop: '15px' }}>
-                        <Card style={{ width: '18rem', textAlign: 'center' }}>
-                            <Card.Img src="./images/product/iphone12pro.png" />
-                            <Card.Body>
-                                <Card.Title>Iphone 12 Pro 128GB</Card.Title>
-                                <Card.Text>
-                                    <Row>
-                                        <Col>26.590.000đ</Col>
-                                    </Row>
-                                    <Row>
-                                        <Col md={6}><Image src="./images/cart/heart.svg" style={{ height: " 1.5 rem" }}></Image></Col>
-                                        <Col md={6}><Image src="./images/product/star.svg" style={{ height: " 25px", width: '25px' }}></Image></Col>
-                                    </Row>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
+                <Row style={{ marginTop: '5px', display: 'flex' }}>
+                    {
+                        product.filter(p => p.categoryId === 1).map(filteredProduct => {
+                            return (
+
+                                <Col xs={12} sm={6} md={3} style={{ marginTop: '15px' }} >
+                                    <Card className='hover:-translate-y-2' style={{ width: '18rem', textAlign: 'center' }}>
+                                        <Card.Img  style={{ height: '290px' }} src={filteredProduct.img} />
+                                        <Card.Body>
+                                            <Card.Title style={{ textAlign: 'center' }}>{filteredProduct.name}</Card.Title>
+                                            <Card.Text>
+                                                <Row>
+                                                    <Col>{filteredProduct.price}</Col>
+                                                </Row>
+                                                <Row>
+                                                    <Col md={6}><Button style={{
+                                                        backgroundColor: 'rgb(220, 100, 11)',
+                                                        color: 'white', fontWeight: 'bolder', fontSize: '14px'
+                                                    }} variant="outline-warning">Detail</Button></Col>
+                                                    <Col md={6}><Button style={{
+                                                        backgroundColor: 'rgb(220, 100, 11)',
+                                                        color: 'white', fontWeight: 'bolder', fontSize: '14px'
+                                                    }} variant="outline-warning">Add To Cart</Button></Col>
+                                                </Row>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+
+                            )
+                        })
+                    }
                 </Row>
-                <Row style={{marginTop:'30px'}}>
+                <Row style={{ marginTop: '45px' }}>
                     <Col md={6}><Button style={{ backgroundColor: 'rgb(220, 100, 11)', color: 'white', fontWeight: 'bolder' }} variant="outline-warning">LAPTOP GAMING</Button></Col>
                     <Col md={6}>
                         <Row>
                             <Col><Button style={{ backgroundColor: 'rgb(220, 100, 11)', color: 'white', fontWeight: 'bolder', fontSize: '14px' }} variant="outline-warning">Asus</Button></Col>
                             <Col><Button style={{ backgroundColor: 'rgb(220, 100, 11)', color: 'white', fontWeight: 'bolder', fontSize: '14px' }} variant="outline-warning">Acer</Button></Col>
-                            <Col><Button style={{ backgroundColor: 'rgb(220, 100, 11)', color: 'white', fontWeight: 'bolder', fontSize: '14px' }} variant="outline-warning">Dell</Button></Col>
+                            <Col><Button style={{ backgroundColor: 'rgb(220, 100, 11)', color: 'white', fontWeight: 'bolder', fontSize: '14px' }} variant="outline-warning">MSI</Button></Col>
                             <Col><Button style={{ backgroundColor: 'rgb(220, 100, 11)', color: 'white', fontWeight: 'bolder', fontSize: '14px' }} variant="outline-warning">HP</Button></Col>
                             <Col><Button style={{ backgroundColor: 'rgb(220, 100, 11)', color: 'white', fontWeight: 'bolder', fontSize: '14px' }} variant="outline-warning">Xem Tất Cả</Button></Col>
                         </Row>
                     </Col>
-                </Row>    
+                </Row>
+                <Row style={{ marginTop: '1px', display: 'flex' }}>
+                    {
+                        product.filter(p => p.categoryId === 5).map(filteredProduct => {
+                            return (
+
+                                <Col xs={12} sm={6} md={3} style={{ marginTop: '15px' }} >
+                                    <Card style={{ width: '18rem', textAlign: 'center' }}>
+                                        <Card.Img hover style={{ height: '280px' }} src={filteredProduct.img} />
+                                        <Card.Body>
+                                            <Card.Title style={{ textAlign: 'center', fontSize: '15px' }}>{filteredProduct.name}</Card.Title>
+                                            <Card.Text>
+                                                <Row>
+                                                    <Col>{filteredProduct.price}</Col>
+                                                </Row>
+                                                <Row>
+                                                    <Col md={6}><Button style={{
+                                                        backgroundColor: 'rgb(220, 100, 11)',
+                                                        color: 'white', fontWeight: 'bolder', fontSize: '14px'
+                                                    }} variant="outline-warning">Detail</Button></Col>
+                                                    <Col md={6}><Button style={{
+                                                        backgroundColor: 'rgb(220, 100, 11)',
+                                                        color: 'white', fontWeight: 'bolder', fontSize: '14px'
+                                                    }} variant="outline-warning">Add To Cart</Button></Col>
+                                                </Row>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+
+                            )
+                        })
+                    }
+                </Row>
+                <Row style={{ marginTop: '45px' }}>
+                    <Col md={6}><Button style={{ backgroundColor: 'rgb(220, 100, 11)', color: 'white', fontWeight: 'bolder' }} variant="outline-warning">TABLET</Button></Col>
+                    <Col md={6}>
+                        <Row>
+                            <Col><Button style={{ backgroundColor: 'rgb(220, 100, 11)', color: 'white', fontWeight: 'bolder', fontSize: '14px' }} variant="outline-warning">Apple</Button></Col>
+                            <Col><Button style={{ backgroundColor: 'rgb(220, 100, 11)', color: 'white', fontWeight: 'bolder', fontSize: '14px' }} variant="outline-warning">Samsung</Button></Col>
+                            <Col><Button style={{ backgroundColor: 'rgb(220, 100, 11)', color: 'white', fontWeight: 'bolder', fontSize: '14px' }} variant="outline-warning">Lenovo</Button></Col>
+                            <Col><Button style={{ backgroundColor: 'rgb(220, 100, 11)', color: 'white', fontWeight: 'bolder', fontSize: '14px' }} variant="outline-warning">OPPO</Button></Col>
+                            <Col><Button style={{ backgroundColor: 'rgb(220, 100, 11)', color: 'white', fontWeight: 'bolder', fontSize: '14px' }} variant="outline-warning">Xem Tất Cả</Button></Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row style={{ marginTop: '1px', display: 'flex' }}>
+                    {
+                        product.filter(p => p.categoryId === 4).map(filteredProduct => {
+                            return (
+
+                                <Col xs={12} sm={6} md={3} style={{ marginTop: '15px' }} >
+                                    <Card style={{ width: '18rem', textAlign: 'center' }}>
+                                        <Card.Img hover style={{ height: '280px' }} src={filteredProduct.img} />
+                                        <Card.Body>
+                                            <Card.Title style={{ textAlign: 'center', fontSize: '15px' }}>{filteredProduct.name}</Card.Title>
+                                            <Card.Text>
+                                                <Row>
+                                                    <Col>{filteredProduct.price}</Col>
+                                                </Row>
+                                                <Row>
+                                                    <Col md={6}><Button style={{
+                                                        backgroundColor: 'rgb(220, 100, 11)',
+                                                        color: 'white', fontWeight: 'bolder', fontSize: '14px'
+                                                    }} variant="outline-warning">Detail</Button></Col>
+                                                    <Col md={6}><Button style={{
+                                                        backgroundColor: 'rgb(220, 100, 11)',
+                                                        color: 'white', fontWeight: 'bolder', fontSize: '14px'
+                                                    }} variant="outline-warning">Add To Cart</Button></Col>
+                                                </Row>
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+
+                            )
+                        })
+                    }
+                </Row>
             </Container>
         </div>
     )
